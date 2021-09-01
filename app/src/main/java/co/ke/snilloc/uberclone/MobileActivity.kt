b9071e6f417df42dc8d5692f4dfa540c493045b7
loop <loopinfinity@criptext.com>
2021-09-01 18:59:27 +0300

    add phone number login ui

--- app/src/main/java/co/ke/snilloc/uberclone/MobileActivity.kt
+++ app/src/main/java/co/ke/snilloc/uberclone/MobileActivity.kt
@@ -1,11 +1,26 @@
 package co.ke.snilloc.uberclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MobileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mobile)
+
+        //set onclick listener on the btn for next
+        val button: Button = findViewById(R.id.MobileNextButton)
+        button.setOnClickListener {
+            val intent = Intent(this, PasswordActivity::class.java)
+            startActivity(intent)
+        }
+        val textView :TextView = findViewById(R.id.MobileSocialTextView)
+        textView.setOnClickListener {
+            val intent = Intent(this, SocialActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/co/ke/snilloc/uberclone/SocialActivity.kt
@@ -0,0 +1,11 @@
+package co.ke.snilloc.uberclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SocialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_social)
+    }
+}
